bc63ebc3c67a40819f530a9c7f20fbde21837e5a
Fran Gil <framicon@gmail.com>
2021-08-19 13:19:54 +0300

    First commit

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React and make it work
         </a>
       </header>
     </div>
