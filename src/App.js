724f6f78f082f477b2f554935ef6218836cbd7d3
Fran Gil <framicon@gmail.com>
2021-08-26 15:00:26 +0300

    First on ft-auth

--- src/App.js
+++ src/App.js
@@ -1,29 +1,8 @@
-//import logo from './logo.svg';
-import Logo from './fr-Logo-3.jpg'
-import './App.css';
+import React from 'react';
+import Providers from './navigation';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={Logo} className="App-logo" alt="logo" />
-        <p><br></br>
-          Web admin <span style={{color:' lime'}}>Ord-App</span> home page.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React and make it work
-        </a>
-        <p>
-          <b>Coming soon: the <span style={{color:'blue', background: 'yellow'}}>RK</span> food ordering web app</b>
-        </p>
-      </header>
-    </div>
-  );
-}
+const App = () => (
+  <Providers />
+);
 
 export default App;
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,15 @@
-import { render, screen } from '@testing-library/react';
+import React from 'react';
+import { render, screen } from './customRender';
 import App from './App';
+/*/import { react } from '@babel/types';
 
 test('renders learn react link', () => {
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
-});
+});*/
+
+test('<App /> rendering', async () => {
+  render(<App />);
+  expect(screen).toMatchSnapshot();
+})
